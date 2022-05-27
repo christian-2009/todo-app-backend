8c558f080c846b53b1f7ce3864e4ad8325a6d44e
christian-2009 <c-sophocleous@hotmail.com>
2022-05-27 15:43:51 +0100

    fixed / route handler

--- server.ts
+++ server.ts
@@ -27,8 +27,8 @@ const client = new Client(dbConfig);
 client.connect();
 
 app.get("/", async (req, res) => {
-  const dbres = await client.query('select * from categories');
-  res.json(dbres.rows);
+  const response = {status: 'listening for requests'};
+  res.json(response);
 });
 
 
